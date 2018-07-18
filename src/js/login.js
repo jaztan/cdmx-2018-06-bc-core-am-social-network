39c6be3f330bb79c0cea0343e38104033a0118f2
Adriana <nossa.musica.07.10@gmail.com>
2018-07-18 01:38:52 -0500

    Solucionando la entrada con usuario ya registrado

--- src/js/login.js
+++ src/js/login.js
@@ -4,18 +4,18 @@ function registrar() {
   let password = document.getElementById("contrasena").value;
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(function () {
-      verificar()
+      .then(function(){
+          verificar()
 
-    })
-    .catch(function (error) {
-      // Handle Errors here.
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-      // ...
-    });
+      })
+      .catch(function (error) {
+          // Handle Errors here.
+          let errorCode = error.code;
+          let errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+          // ...
+      });
 }
 
 function ingresar() {
@@ -23,76 +23,75 @@ function ingresar() {
   let password2 = document.getElementById("contrasena2").value;
 
   firebase.auth().signInWithEmailAndPassword(email2, password2)
-    .catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      console.log(errorMessage);
-      // ...
-    });
+      .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+          // ...
+      });
 }
 
 function observador() {
   firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      aparece(user);
-      // User is signed in.
-      let displayName = user.displayName;
-      let email = user.email;
-      console.log('***************');
-      console.log(user.emailVerified);
-      console.log('***************');
-      let emailVerified = user.emailVerified;
-      let photoURL = user.photoURL;
-      let isAnonymous = user.isAnonymous;
-      let uid = user.uid;
-      let providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      console.log("No existe usuario, registrese para acceder");
-      // ...
-    }
+      if (user) {
+          //aparece(user);
+          // User is signed in.
+          let displayName = user.displayName;
+          let email = user.email;
+          // console.log('***************');
+          console.log(user.emailVerified);
+          // console.log('***************');
+          let emailVerified = user.emailVerified;
+          let photoURL = user.photoURL;
+          let isAnonymous = user.isAnonymous;
+          let uid = user.uid;
+          let providerData = user.providerData;
+          // ...
+      } else {
+          // User is signed out.
+          console.log("No existe usuario, registrese para acceder");
+          // ...
+      }
   });
 
 }
 
 observador();
-
+/*
 function aparece(user) {
-  let users = user;
+  let user = user;
   let contenido = document.getElementById("contenido");
-  if (user.emailVerified) {
-    contenido.innerHTML =
+  if(user.emailVerified){
+      contenido.innerHTML =
       `
   <p>Bienvenido</p>
 
   <button onclick="cerrar()">Cerrar sesión</button>
   `;
-
   }
-}
+}*/
 
 function cerrar() {
   firebase.auth().signOut()
-    .then(function () {
-      console.log("Saliendo...")
-    })
-    .catch(function (error) {
-      console.log(error)
-    });
+      .then(function () {
+          console.log("Saliendo...")
+      })
+      .catch(function (error) {
+          console.log(error)
+      });
 }
 
 function verificar() {
   let user = firebase.auth().currentUser;
 
   user.sendEmailVerification().then(function () {
-    console.log('Enviando correo de verificación...');
-    // Email sent.
+      console.log('Enviando correo de verificación...');
+      // Email sent.
   }).catch(function (error) {
-    console.log(error);
-    // An error happened.
+      console.log(error);
+      // An error happened.
   });
 
 }
