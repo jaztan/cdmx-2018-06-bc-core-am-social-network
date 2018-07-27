e62e340352838b173ac7c9db8ed782ba7d8aac1e
jaztan <jaztangr@outlook.com>
2018-07-27 10:28:30 -0500

    comprobando funcionalidad de inicio sesion con facebook

--- src/js/login.js
+++ src/js/login.js
@@ -151,6 +151,7 @@ const validacion=(provider)=>{
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
+        console.log("Iniciando Sesion con Facebook")
         // ...
       }).catch(function(error) {
         // Handle Errors here.
