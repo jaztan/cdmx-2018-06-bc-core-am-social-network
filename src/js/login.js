2044204d93d7a0ef30b5a113986c4e78f51acb96
jaztan <jaztangr@outlook.com>
2018-07-27 10:48:57 -0500

    redireccionando ingreso de facebook a muro

--- src/js/login.js
+++ src/js/login.js
@@ -147,7 +147,9 @@ validacion(provider);
 
 const validacion=(provider)=>{
     firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    
+      location.href = "../views/wall.html";
+       //location es un metodo
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
