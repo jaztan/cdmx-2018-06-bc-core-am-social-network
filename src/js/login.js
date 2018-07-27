05405eaea2edc3753e1e8dabc1bc20ddbaffe9d4
jaztan <jaztangr@outlook.com>
2018-07-27 08:33:29 -0500

    funcion login facebook

--- src/js/login.js
+++ src/js/login.js
@@ -136,3 +136,32 @@ function verificar() {
       // An error happened.
     });
 }
+
+const btnFace=document.getElementById('btnFace');
+
+
+btnFace.addEventListener('click',function(e){
+let provider = new firebase.auth.FacebookAuthProvider();
+validacion(provider);
+});
+
+const validacion=(provider)=>{
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
+   
+
