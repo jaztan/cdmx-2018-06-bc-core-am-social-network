47a8bd6b036a946b7edc9895ba49f12299ff483f
jaztan <jaztangr@outlook.com>
2018-07-28 10:09:20 -0500

    probando ingreso de sesion con celular

--- src/js/login.js
+++ src/js/login.js
@@ -169,3 +169,27 @@ validacion(provider);
 //}
    
 
+firebase.auth().languageCode = 'it';
+// To apply the default browser preference instead of explicitly setting it.
+// firebase.auth().useDeviceLanguage();
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('captcha');
+var phoneNumber = getPhoneNumberFromUserInput('phone');
+var appVerifier = window.recaptchaVerifier;
+firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then(function (confirmationResult) {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      window.confirmationResult = confirmationResult;
+    }).catch(function (error) {
+      // Error; SMS not sent
+      // ...
+    });
+    var code = getCodeFromUserInput('widgetId');
+confirmationResult.confirm(code).then(function (result) {
+  // User signed in successfully.
+  var user = result.user;
+  // ...
+}).catch(function (error) {
+  // User couldn't sign in (bad verification code?)
+  // ...
+});
