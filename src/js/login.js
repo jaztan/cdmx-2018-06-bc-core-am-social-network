587691132799d5f05b5f50230394eb47aef006d8
Jazmin Tania Reyes Guzman <39322976+jaztan@users.noreply.github.com>
2018-07-29 18:46:13 -0500

    probando  funcion  complementaria de facebook

--- src/js/login.js
+++ src/js/login.js
@@ -168,5 +168,15 @@ validacion(provider);
       });
 //}
    
+ (function(d, s, fb-root) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById()) return;
+    js = d.createElement(s); js.id = fb-root;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
+
+ 
+</html>
 
