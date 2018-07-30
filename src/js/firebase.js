d37af6495800e8419846664e3234518cb10392ff
Adriana <nossa.musica.07.10@gmail.com>
2018-07-29 19:29:43 -0500

    Archivo con la información de Farebase y funciones de Login

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -8,3 +8,121 @@ window.initializeFirebase = () => {
     messagingSenderId: "578346489088"
   });
 };
+
+window.countMeNetwork = {
+
+  registerNewAccount: (email, password) => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(result => {
+
+        //verififyAccount();
+        location.href = "../index.html";
+      })
+      .catch(error => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode);
+        alert(errorMessage);
+        // ...
+      });
+  },
+
+  signInUser: (email, password) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(event => {
+        // Se utiliza la interfaz Location, implementando la propiedad Location.href que contiene la URL
+        location.href = "../views/wall.html";
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode);
+        alert(errorMessage);
+      });
+  },
+
+  signInGoogle: () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        location.href = "../views/wall.html";
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        console.log(result)
+      }).catch(error => {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        let email = error.email;
+        let credential = error.credential;
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+          alert('Usuario ya existente, intenta de nuevo');
+        }
+      });
+  },
+
+
+  verififyAccount: () => {
+    let user = firebase.auth().currentUser;
+    user
+      .sendEmailVerification()
+      .then(result => {
+        alert("Enviando correo de verificación...");
+        // Email sent.
+      })
+      .catch(function (error) {
+        console.log(error);
+        // An error happened.
+      });
+  },
+
+  signOut: () => {
+    firebase.auth().signOut()
+      .then(event => {
+        location.href('../index.html');
+        alert('Saliendo...')
+      }).catch(error => {
+        console.log('Error al cerrar sesión');
+      });
+  }
+
+}
+
+/*
+const btnFace=document.getElementById('btnFace');
+
+
+btnFace.addEventListener('click',function(e){
+e.preventDefault();
+let provider = new firebase.auth.FacebookAuthProvider();
+validacion(provider);
+});
+
+//const validacion=(provider)=>{
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    console.log(result);
+    window.location.assign("../views/wall.html");
+     //location es un metodo
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log("Iniciando Sesion con Facebook")
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+//}
+*/
