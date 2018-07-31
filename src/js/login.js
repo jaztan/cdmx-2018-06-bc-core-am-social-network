38219fb0d95ae2a45eaacf14b666e229b615c23d
jaztan <jaztangr@outlook.com>
2018-07-31 15:04:39 -0500

    redireccionando facebook a muro

--- src/js/login.js
+++ src/js/login.js
@@ -4,15 +4,15 @@ btnFace.addEventListener('click', function (e) {
   validacion(provider);
 });
 const validacion = (provider) => {
-  firebase.auth().signInWithPopup(provider).then(function (result) {
+  firebase.auth().signInWithPopup(provider).then(result => {
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
     // ...
-    window.location.assign("../views/wall.html");
+    window.location.assign('../views/wall.html');
     
-  }).catch(function (error) {
+}).catch(error => {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -21,4 +21,4 @@ const validacion = (provider) => {
     // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
   });
-}
+};
