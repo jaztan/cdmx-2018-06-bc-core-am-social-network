8f4dbb51437047fc4bb66dca396fd55502e635ef
jaztan <jaztangr@outlook.com>
2018-07-31 18:13:03 -0500

    facebook id

--- src/js/facebook.js
+++ src/js/facebook.js
@@ -1,10 +1,10 @@
-const btnFace = document.getElementById('btnFace');
+const btnFace = document.getElementById('facebook-button');
 btnFace.addEventListener('click', function (e) {
   let provider = new firebase.auth.FacebookAuthProvider();
   validacion(provider);
 });
 const validacion = (provider) => {
-  firebase.auth().signInWithPopup(provider).then(function (result) {
+  firebase.auth().signInWithPopup(provider).then(result=> {
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var token = result.credential.accessToken;
     // The signed-in user info.
